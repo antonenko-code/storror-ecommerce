bbdf87272d1994126c3feb2c977e928f9eda08f9
Oleksandr Antonenko <alexandr.antonenko.fr@gmail.com>
2024-11-04 01:52:10 +0200

    chore: favicons, manifest.json and general metadata are added

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 import "@styles/globals.css";
 
 export const metadata: Metadata = {
-  title: "STORROR® | Clothing & Technical sportswear",
+  title: {
+    template: "%s | Storror",
+    default: "STORROR® | Clothing & Technical sportswear",
+  },
   description:
     "Discover STORROR®'s exclusive range of clothing, shoes, and accessories, designed by world-class parkour athletes. Shop now for premium gear!",
 };
@@ -14,6 +17,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="apple-mobile-web-app-title" content="Storror" />
+        <meta name="theme-color" content="#1B1B1B" />
+        <meta name="background-color" content="#ffffff" />
+      </head>
       <body>{children}</body>
     </html>
   );
